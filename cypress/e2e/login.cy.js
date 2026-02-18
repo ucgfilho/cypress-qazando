01c8d2936ed92d4c8f1380ce265a87bc63768903
Ubirajara Filho <110053911+ucgfilho@users.noreply.github.com>
2026-02-18 10:51:23 -0300

    fix: strict-inequality improvement

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,36 +1,37 @@
-import { faker } from "@faker-js/faker";
+import {faker} from "@faker-js/faker";
 import homePage from "../support/pages/homePage";
 import loginPage from "../support/pages/loginPage";
+
 const resolutions = require("../fixtures/resolutions.json");
 
 resolutions.forEach((resolution) => {
-  describe("Login", () => {
-    const email = faker.internet.email();
-    const invalidEmail = faker.internet.email().replace("@", "");
-    const password = faker.internet.password(6);
-    const passwordInvalid = faker.string.alphanumeric(5);
+    describe("Login", () => {
+        const email = faker.internet.email();
+        const invalidEmail = faker.internet.email().replace("@", "");
+        const password = faker.internet.password(6);
+        const passwordInvalid = faker.string.alphanumeric(5);
 
-    beforeEach(() => {
-      if (resolution != "desktop") {
-        cy.viewport(resolution);
-      }
+        beforeEach(() => {
+            if (resolution !== "desktop") {
+                cy.viewport(resolution);
+            }
 
-      homePage.accessLoginPage();
+            homePage.accessLoginPage();
+        });
+
+        it("Login com credenciais válidas", () => {
+            loginPage.loginSuccessful(email, password);
+            loginPage.validateLoginSuccessful(email);
+        });
+
+        it("Login com senha inválida", () => {
+            loginPage.loginWithInvalidPassword(email, passwordInvalid);
+            loginPage.validateInvalidPassword();
+        });
+
+        it("Login com email inválido", () => {
+            loginPage.loginWithInvalidEmail(invalidEmail, password);
+            loginPage.validateInvalidEmail();
+        });
     });
-
-    it("Login com credenciais válidas", () => {
-      loginPage.loginSuccessful(email, password);
-      loginPage.validateLoginSuccessful(email);
-    });
-
-    it("Login com senha inválida", () => {
-      loginPage.loginWithInvalidPassword(email, passwordInvalid);
-      loginPage.validateInvalidPassword();
-    });
-
-    it("Login com email inválido", () => {
-      loginPage.loginWithInvalidEmail(invalidEmail, password);
-      loginPage.validateInvalidEmail();
-    });
-  });
 });
